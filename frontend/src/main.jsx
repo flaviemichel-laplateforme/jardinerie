f787479729d28f92b8311619283ab6f8fbbd3918
flaviemichel-laplateforme <flavie.michel@laplateforme.io>
2026-06-22 11:12:22 +0200

    feat(AuthContext): ajout du contexte d'authentification et gestion de la session utilisateur

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,14 +1,18 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-import { CartProvider } from './contexts/CartContext.jsx' 
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import App from './App.jsx';
+import { CartProvider } from './contexts/CartContext.jsx';
+import { AuthProvider } from './contexts/AuthContext.jsx';
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     {/* 2. On enveloppe l'application entière pour que le panier soit accessible partout */}
-    <CartProvider>
-      <App />
-    </CartProvider>
+    <AuthProvider>
+      <CartProvider>
+        <App />
+      </CartProvider>
+    </AuthProvider>
   </StrictMode>,
 )
